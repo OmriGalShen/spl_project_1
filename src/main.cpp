52664be0520a64fd837ed71214f69d1d8daff8f9
Omri Gal Shenhav <shenhav.omri@gmail.com>
2020-10-31 23:00:17 +0200

    In order to read input json files added nlohmann/json.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,20 @@
 #include "Graph.h"
 #include "Agent.h"
 #include "Tree.h"
+#include <nlohmann/json.hpp>
 
 using namespace std;
 
-int main(int argc, char** argv){
-    // if(argc != 2){
-    //     cout << "usage cTrace <config_path>" << endl;
-    //     return 0;
-    // }
+
+int main(int argc, char** argv)
+{
+     if(argc != 2) // json file for testing was given
+     {
+         cout << "usage cTrace <config_path>" << endl;
+         return 0;
+     }
     // Session sess(argv[1]);
     // sess.simulate();
-
     cout<< "Hello World!" << endl;
 
     return 0;
