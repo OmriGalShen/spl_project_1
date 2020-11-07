a68adcc533a5092a6ed080c5f9c6839d6516d2e1
Omri Gal Shenhav <shenhav.omri@gmail.com>
2020-11-08 01:32:18 +0200

    Implemented traceTree() for MaxRankTree

--- src/classes/trees/MaxRankTree.cpp
+++ src/classes/trees/MaxRankTree.cpp
@@ -1,7 +1,5 @@
 #include "../../../include/Tree.h"
 #include <deque>
-#include "iostream"
-using namespace std;
 
 MaxRankTree::MaxRankTree(int rootLabel)
         : Tree(rootLabel)
@@ -9,32 +7,32 @@ MaxRankTree::MaxRankTree(int rootLabel)
 
 }
 
+MaxRankTree::MaxRankTree(const MaxRankTree &other):Tree(other) {}
+
 Tree * MaxRankTree::clone() const
 {
-    return new MaxRankTree(node);
+    return new MaxRankTree((*this));
 }
 
 int MaxRankTree::traceTree()
 {
-//    int maxChildrenCount = 0;
-//    MaxRankTree* maxChildrenPtr = this;
-//    std::deque<MaxRankTree*> nodesToCheck;
-//    nodesToCheck.push_back(this);
-//    while (!nodesToCheck.empty())
-//    {
-//        MaxRankTree* curr = nodesToCheck.front();
-//        nodesToCheck.pop_front();
-//        cout << "Pop: " << (*curr).node <<endl;
-//        auto currChildren = ((MaxRankTree)(*curr)).children;
-//        auto currentChildrenCount = currChildren.size();
-//        cout << "currentChildrenCount: " << (*curr).children.size() <<endl;
-//        if(currentChildrenCount>maxChildrenCount)
-//        {
-//            maxChildrenCount=currentChildrenCount;
-//            maxChildrenPtr = curr;
-//        }
-//        for(auto* child: currChildren)
-//            nodesToCheck.push_back((MaxRankTree*)child);
-//    }
-//    std::cout << "Max Rank:" << (*maxChildrenPtr).node << endl;
+    int maxChildrenCount = 0;
+    MaxRankTree* maxChildrenPtr = this;
+    std::deque<MaxRankTree*> nodesToCheck;
+    nodesToCheck.push_back(this);
+    while (!nodesToCheck.empty())
+    {
+        MaxRankTree* curr = nodesToCheck.front();
+        nodesToCheck.pop_front();
+        auto currChildren = ((MaxRankTree)(*curr)).children;
+        auto currentChildrenCount = currChildren.size();
+        if(currentChildrenCount>maxChildrenCount)
+        {
+            maxChildrenCount=currentChildrenCount;
+            maxChildrenPtr = curr;
+        }
+        for(auto* child: currChildren)
+            nodesToCheck.push_back((MaxRankTree*)child);
+    }
+    return (*maxChildrenPtr).node;
 }
