48a0e3d1f72242733a16be9ba9075db74e9ba80d
Omri Gal Shenhav <shenhav.omri@gmail.com>
2020-11-08 18:20:51 +0200

    implemented ContactTracer act()

--- src/classes/agents/ContactTracer.cpp
+++ src/classes/agents/ContactTracer.cpp
@@ -1,4 +1,5 @@
 #include "../../../include/Agent.h"
+#include "iostream"
 
 ContactTracer::ContactTracer(Session& session): Agent(session)
 {
@@ -7,7 +8,14 @@ ContactTracer::ContactTracer(Session& session): Agent(session)
 
 void ContactTracer::act()
 {
-
+    int infectedNode = session.dequeueInfected();
+    Tree* shortPathTree = session.BFS(infectedNode);
+    int nodeToRemove = shortPathTree->traceTree();
+//    std::cout << "----------------------"<< std::endl;
+//    std::cout << "IN CONTRACT TRACER" << std::endl;
+//    std::cout<<"nodeToRemove:" << nodeToRemove << std::endl;
+//    std::cout << "----------------------"<< std::endl;
+    session.removeNode(nodeToRemove);
 }
 
 Agent * ContactTracer::clone()  const
