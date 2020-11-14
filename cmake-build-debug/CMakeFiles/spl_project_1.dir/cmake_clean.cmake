cbc8107415530f48cb968cc22aea1fa9134aa6df
Eden <edenholin@gmail.com>
2020-11-14 19:10:06 +0200

    can finally debug (updated to master) :)

---
+++ cmake-build-debug/CMakeFiles/spl_project_1.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spl_project_1.dir/src/classes/Graph.cpp.o"
+  "CMakeFiles/spl_project_1.dir/src/classes/Session.cpp.o"
+  "CMakeFiles/spl_project_1.dir/src/classes/agents/Agent.cpp.o"
+  "CMakeFiles/spl_project_1.dir/src/classes/agents/ContactTracer.cpp.o"
+  "CMakeFiles/spl_project_1.dir/src/classes/agents/Virus.cpp.o"
+  "CMakeFiles/spl_project_1.dir/src/classes/trees/CycleTree.cpp.o"
+  "CMakeFiles/spl_project_1.dir/src/classes/trees/MaxRankTree.cpp.o"
+  "CMakeFiles/spl_project_1.dir/src/classes/trees/RootTree.cpp.o"
+  "CMakeFiles/spl_project_1.dir/src/classes/trees/Tree.cpp.o"
+  "CMakeFiles/spl_project_1.dir/src/main.cpp.o"
+  "spl_project_1"
+  "spl_project_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spl_project_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
